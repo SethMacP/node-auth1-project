45b416dd2161ef4f523f1cc454f2488c42501f17
SethMacP <macpherson.seth@gmail.com>
2021-03-10 17:25:33 -0800

    mvp met. Tests are broken but all testing points worked when tested in insomnia. Verfified with a TL

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,5 @@
+const usersModel = require('../users/users-model')
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,22 +8,31 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+const restricted =  () => async(req, res, next) => {
+	try{
+		if(!req.session || !req.session.chocolatechip){
+			return res.status(401).json({message:"You shall not pass!"})
+		}	
+		next();
+	}catch(err){
+		next(err);
+	}
 }
 
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
-function checkUsernameFree() {
-
+//used for registration
+const checkUsernameFree = () => async(req, res, next) => {
+	try{
+		const {username} = req.body
+		const user = await usersModel.findBy({username})
+		// console.log('user: ',user)
+		if(user){
+			return res.status(422).json({message:"Username taken"})
+		}
+		next();
+	}catch(err){
+		next();
+	}
 }
-
 /*
   If the username in req.body does NOT exist in the database
 
@@ -30,8 +41,18 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+//used for login?
+const checkUsernameExists =  () => async(req, res, next) => {
+	try{
+		const {username} = req.body;
+		const check = await usersModel.findBy({username});
+			if(!check){
+				return res.status(401).json({message: "Invalid credentials"})
+			}
+		next();
+	}catch(err){
+		next(err)
+	}
 }
 
 /*
@@ -42,8 +63,26 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+const checkPasswordLength =  () => async(req, res, next) => {
+	try{
+		// console.log('password middleware ran')
+		const {password} = req.body;
+		// console.log('password length: ', password.length)
+		if(!password || password.length < 4){
+			return res.status(422).json({message: "Password must be longer than 3 chars"})
+		}
+		next();
+	}catch(err){
+	next()
+	}
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+
+
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+}
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,30 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const {checkUsernameFree, checkPasswordLength, checkUsernameExists} = require('./auth-middleware');
+const usersModel = require('../users/users-model')
+const bcrypt = require('bcryptjs');
+const { json } = require('express');
+
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 
-
-/**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
+router.post("/register", checkUsernameFree(), checkPasswordLength(),  async(req,res,next)=>{
+  try{
+	//   console.log("Router Reached")
+      const {username, password} = req.body;
+      const addUser = await usersModel.add({
+        username: username,
+        password: await bcrypt.hash(password, 2)
+      })
+      res.status(201).json(addUser)
+  }catch(err){
+    next(err)
   }
+})
 
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
-  }
+  router.post("/login", checkUsernameExists(), async (req,res,next) =>{
+    try{
+		  // console.log("Router Reached")
+		const {username, password} = req.body
+		  // console.log(username, password, )
+		const user = await usersModel.findBy({username})
+		  // console.log("user", user)
+		const passwordCheck = await bcrypt.compare(password, user.password)
+		  // console.log("passwordCheck: ", passwordCheck)
+		if (!user || passwordCheck === false ){
+			return res.status(401).json({message: "Invalid credentials"})
+		}
 
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
- */
-
+		req.session.chocolatechip = user;
+		res.json({message: `Welcome ${user.username}`})
+		// console.log('success')
+  
+    }catch(err){
+      next(err)
+    }
+  })
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -41,7 +59,23 @@
     "message": "Invalid credentials"
   }
  */
-
+  router.get("/logout", async(req,res,next)=>{
+    try{
+      console.log(req.session)
+      if(!req.session.chocolatechip){
+        return res.status(200).json({message:"no session"})
+      }
+			req.session.destroy(err=>{
+				if(err){
+					next(err)
+				}else{
+					res.status(200).json({message:"logged out"})
+				}
+			})
+    }catch(err){
+      next(err)
+    }
+  })
 
 /**
   3 [GET] /api/auth/logout
@@ -61,3 +95,4 @@
 
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
